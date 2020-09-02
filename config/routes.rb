f92d506d82d1f267b4a60fc7236be64864c54f2b
ksk-o <69468089+ksk-o@users.noreply.github.com>
2020-09-02 19:14:23 +0900

    チャットルームの削除機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "rooms#index"
   resources :users, only: [:edit, :update]
-  resources :rooms, only: [:new, :create] do
+  resources :rooms, only: [:new, :create, :destroy] do
     resources :messages, only: [:index, :create]
     #ルーティングのネスト 入れ子構造 どのルームのメッセージを見るのか、作るのかという設定
     #メッセージに結びつくルームのidの情報を含んだパスを、受け取れるようになる
